03c84aeb5377a8b6ab02e2e96478b2c069fe0706
lawrence alkhalaileh <lorans0alkhalailah@gmail.com>
2025-09-09 02:07:45 +0300

    server sidebar boilerplate

---
+++ src/components/server/server-sidebar.tsx
@@ -0,0 +1,71 @@
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { ChannelType } from "@prisma/client";
+import { redirect } from "next/navigation";
+import React from "react";
+import ServerHeader from "./server-header";
+
+interface ServerSidebarProps {
+  serverId: string;
+}
+
+const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirect("/");
+  }
+
+  const server = await db.server.findUnique({
+    where: {
+      id: serverId,
+    },
+    include: {
+      Channel: {
+        orderBy: {
+          createdAt: "asc",
+        },
+      },
+      Member: {
+        include: {
+          profile: true,
+        },
+        orderBy: {
+          role: "asc",
+        },
+      },
+    },
+  });
+
+  if (!server) {
+    return redirect("/");
+  }
+
+  const textChannels = server?.Channel.filter(
+    (channel) => channel.type === ChannelType.TEXT
+  );
+
+  const videoChannels = server?.Channel.filter(
+    (channel) => channel.type === ChannelType.VIDEO
+  );
+
+  const audioChannels = server?.Channel.filter(
+    (channel) => channel.type === ChannelType.AUDIO
+  );
+
+  const members = server?.Member.filter(
+    (member) => member.profileId !== profile.id
+  );
+
+  const role = server.Member.find(
+    (member) => member.profileId === profile.id
+  )?.role;
+
+  return (
+    <div className="flex flex-col h-full text-primary w-full dark:bg-[#2B2D31] bg-[#F2F3F5]">
+      <ServerHeader server={server} role={role} />
+    </div>
+  );
+};
+
+export default ServerSidebar;
